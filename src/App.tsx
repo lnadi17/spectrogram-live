82f3c4be617856b3563a5b8a12d9452209a1b94a
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2023-06-18 06:14:42 +0400

    Create initial React project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import {Box, ChakraProvider, Grid, Spinner} from "@chakra-ui/react";
+
+function App() {
+    return (
+        <div></div>
+    );
+}
+
+export default App;
