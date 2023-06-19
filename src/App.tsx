8a8014e7814defe0bf67a4405780df9781f4591b
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2023-06-19 08:17:44 +0400

    Make layout responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,36 @@ import './App.css';
 import {Box, Container, Grid, GridItem, Heading, Spacer, Spinner, Text} from "@chakra-ui/react";
 
 function App() {
+    const fs = {base: 'sm', md: 'lg', lg: 'xl'};
     return (
-        <Grid templateColumns="repeat(6, 1fr)" bg="gray.50" h={"100%"}>
-            <GridItem
+        <Box h={"100vh"} display={"flex"} flexDir={"column"}>
+            <Box
                 as="aside"
                 bg={"gray.500"}
-                colSpan={6}
-                h={"20vh"}>
-                <Heading as="h1" m={2} color={"blue.50"}>Spectrogram Live</Heading>
-                <Text ml={3} color={"blue.100"}>A web application developed by Luka Nadiradze during the Digital Signal
+                w={"100vw"}>
+                <Heading m={2} color={"blue.50"}>Spectrogram Live</Heading>
+                <Text ml={3} color={"blue.100"} fontSize={fs}>A web application developed by Luka Nadiradze during
+                    the Digital Signal
                     Processing course.</Text>
-                <Text ml={3} mb={1} color={"blue.100"}>Record the sound from your microphone and see what frequencies the
+                <Text ml={3} mb={1} color={"blue.100"} fontSize={fs}>Record the sound from your microphone and see
+                    what frequencies the
                     sound consists of.</Text>
-            </GridItem>
-            <GridItem
-                as="main"
-                bg={"purple.700"}
-                colSpan={4}>
-                <Box height="80vh" display="flex" flexDirection="column">
+            </Box>
+            <Grid templateColumns="repeat(6, 1fr)" bg="gray.50" flexGrow={1}>
+                <GridItem
+                    as="main"
+                    bg={"purple.700"}
+                    colSpan={{base: 6, md: 4}}>
                     <Text>Spectrogram here</Text>
-                </Box>
-            </GridItem>
-            <GridItem
-                as={"aside"}
-                colSpan={2}
-                bg={"gray.600"}>
-                <Text>Settings Here</Text>
-            </GridItem>
-        </Grid>
+                </GridItem>
+                <GridItem
+                    as={"aside"}
+                    colSpan={{base: 6, md: 2}}
+                    bg={"gray.600"}>
+                    <Text>Settings Here</Text>
+                </GridItem>
+            </Grid>
+        </Box>
     );
 }
 
