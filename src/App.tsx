e45ef307e6ca38314697adc31929cf39d57cf9c4
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2023-06-19 08:45:05 +0400

    Add part of the settings layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import './App.css';
-import {Box, Container, Grid, GridItem, Heading, Spacer, Spinner, Text} from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Container, Divider,
+    Grid,
+    GridItem,
+    Heading,
+    Radio,
+    RadioGroup,
+    Spacer,
+    Spinner, Stack, StackDivider,
+    Text
+} from "@chakra-ui/react";
 
 function App() {
     const fs = {base: 'sm', md: 'lg', lg: 'xl'};
@@ -8,8 +20,7 @@ function App() {
         <Box h={"100vh"} display={"flex"} flexDir={"column"}>
             <Box
                 as="aside"
-                bg={"gray.500"}
-                w={"100vw"}>
+                bg={"gray.500"}>
                 <Heading m={2} color={"blue.50"}>Spectrogram Live</Heading>
                 <Text ml={3} color={"blue.100"} fontSize={fs}>A web application developed by Luka Nadiradze during
                     the Digital Signal
@@ -20,16 +31,43 @@ function App() {
             </Box>
             <Grid templateColumns="repeat(6, 1fr)" bg="gray.50" flexGrow={1}>
                 <GridItem
-                    as="main"
                     bg={"purple.700"}
-                    colSpan={{base: 6, md: 4}}>
+                    colSpan={{base: 6, md: 4}}
+                    p={5}>
                     <Text>Spectrogram here</Text>
                 </GridItem>
                 <GridItem
-                    as={"aside"}
                     colSpan={{base: 6, md: 2}}
-                    bg={"gray.600"}>
-                    <Text>Settings Here</Text>
+                    bg={"gray.600"} p={5}>
+                    <Button colorScheme='teal' variant='outline'>
+                        Start Recording
+                    </Button>
+                    <Text>Perspective</Text>
+                    <RadioGroup defaultValue='2' m={2}>
+                        <Stack spacing={5} direction='row'>
+                            <Radio colorScheme='red' value='1'>
+                                3D
+                            </Radio>
+                            <Radio colorScheme='green' value='2'>
+                                2D
+                            </Radio>
+                        </Stack>
+                    </RadioGroup>
+                    <Text>Frequency Scale</Text>
+                    <RadioGroup defaultValue='2' m={2}>
+                        <Stack spacing={5} direction='row'>
+                            <Radio colorScheme='red' value='1'>
+                                Logarithmic
+                            </Radio>
+                            <Radio colorScheme='green' value='2'>
+                                Linear
+                            </Radio>
+                        </Stack>
+                    </RadioGroup>
+                    <Text>Frequency Resolution</Text>
+                    <Text>Time Resolution</Text>
+                    <Text>Time Samples Overlap</Text>
+                    <Text>Window Function</Text>
                 </GridItem>
             </Grid>
         </Box>
