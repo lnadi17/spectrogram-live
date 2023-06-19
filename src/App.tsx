ade2e59728c82d85ad0c2a8853a39c336f09cd7a
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2023-06-19 07:53:52 +0400

    Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import './App.css';
-import {Box, ChakraProvider, Container, Grid, GridItem, Spinner} from "@chakra-ui/react";
-import theme from "./theme";
+import {Box, Container, Grid, GridItem, Heading, Spacer, Spinner, Text} from "@chakra-ui/react";
 
 function App() {
     return (
-        <Container >
-            Here begins my app.
-        </Container>
+        <Grid templateColumns="repeat(6, 1fr)" bg="gray.50" h={"100%"}>
+            <GridItem
+                as="aside"
+                bg={"gray.500"}
+                colSpan={6}
+                h={"20vh"}>
+                <Heading as="h1" m={2} color={"blue.50"}>Spectrogram Live</Heading>
+                <Text ml={3} color={"blue.100"}>A web application developed by Luka Nadiradze during the Digital Signal
+                    Processing course.</Text>
+                <Text ml={3} mb={1} color={"blue.100"}>Record the sound from your microphone and see what frequencies the
+                    sound consists of.</Text>
+            </GridItem>
+            <GridItem
+                as="main"
+                bg={"purple.700"}
+                colSpan={4}>
+                <Box height="80vh" display="flex" flexDirection="column">
+                    <Text>Spectrogram here</Text>
+                </Box>
+            </GridItem>
+            <GridItem
+                as={"aside"}
+                colSpan={2}
+                bg={"gray.600"}>
+                <Text>Settings Here</Text>
+            </GridItem>
+        </Grid>
     );
 }
 
